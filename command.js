42b09c8e1795480934300395dfef364398ecd053
SaKagiRi <kasichonooo@gmail.com>
2024-06-28 11:56:40 +0700

    nice
    ระบบหลังบ้านได้ละ เหลื่อจัดการCss and animetion เพิ่มเติม

--- command.js
+++ command.js
@@ -1,12 +1,21 @@
 const  btn = document.getElementById('btn');
 const body = document.querySelector('body');
+const colorIn = document.getElementById('addColor');
+const btn2 = document.getElementById('btn2');
 
-var color = ['red','blue','green','yello','pink','purple'];
-alert(color.length);
 
-for (let i = 0;i != color.length;i++) {
-    btn.onclick = function() {
-        alert(i);
+var color = ['red','blue','green','yellow','pink','purple'];
+var i = 0;
+
+btn2.onclick = function() {
+    color.push(colorIn.value);
+}
+
+btn.onclick = function() {
         body.style.backgroundColor = color[i];
+        i++
+
+    if (i == color.length) {
+        i = 0;
     }
 }
