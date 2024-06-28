2cc3c5f346f002dfc0f5ef3e6f6ba9ae78cd8f41
SaKagiRi <kasichonooo@gmail.com>
2024-06-28 21:30:25 +0700

    hmm
    ช่างมันละกันโปรเจ็คนี้หาโปรเจ็คต่อไปทำดีกว่า

--- command.js
+++ command.js
@@ -2,6 +2,8 @@ const  btn = document.getElementById('btn');
 const body = document.querySelector('body');
 const colorIn = document.getElementById('addColor');
 const btn2 = document.getElementById('btn2');
+const txt = document.getElementById('text');
+
 
 
 var color = ['red','blue','green','yellow','pink','purple'];
@@ -11,10 +13,12 @@ btn2.onclick = function() {
     color.push(colorIn.value);
 }
 
+
 btn.onclick = function() {
         body.style.backgroundColor = color[i];
+        txt.innerHTML = '<h1>'+color+'</h1>';
         i++
-
+        
     if (i == color.length) {
         i = 0;
     }
